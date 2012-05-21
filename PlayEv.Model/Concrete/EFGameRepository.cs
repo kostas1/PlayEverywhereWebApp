41f1b0d910745153703701d35ec57d244ab5a38c
7zxc <alkenchuga@gmail.com>
2012-05-21 13:08:26 +0200

    Games

---
+++ PlayEv.Model/Concrete/EFGameRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PlayEv.Model.Abstract;
+
+namespace PlayEv.Model.Concrete
+{
+    public class EFGameRepository : IGameRepository
+    {
+        private EFDbContext context = new EFDbContext();
+        public IQueryable<Entities.Game> Games
+        {
+            get { return context.Games; }
+        }
+    }
+}
